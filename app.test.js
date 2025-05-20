731810cdde84c805a0114aa0faf6a5ca2b1bc483
Yaddaden Yacine <yaddya01@uqar.ca>
2025-05-20 13:58:34 -0400

    update: add the source code.

---
+++ app.test.js
@@ -0,0 +1,15 @@
+const { add } = require("./app");
+
+describe("Addition Function", () => {
+  test("adds 2 + 3 to equal 5", () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  test("adds -1 + 1 to equal 0", () => {
+    expect(add(-1, 1)).toBe(0);
+  });
+
+  test("adds 0 + 0 to equal 0", () => {
+    expect(add(0, 0)).toBe(0);
+  });
+});
